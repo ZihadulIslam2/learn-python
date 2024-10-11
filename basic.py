8fdf9430eeefab04f0415b761c1cfdfb1362639f
zihadulislam2 <zihadul708@gmail.com>
2024-10-11 10:55:32 +0600

    c2_greeting_app

---
+++ basic.py
@@ -0,0 +1,37 @@
+# # execution order
+# print('hello python line-1')
+# print('hello python line-2')
+# print('hello python line-3')
+
+# # datatype
+# print("hello") #string
+# print('hello')
+# print(123) # integer
+# print(-10)
+# print(1.5) # floting point value / flot
+
+# # oparations 
+# print(3+3)
+
+# print(3-3)
+# print(3*3)
+# print(3/3)
+
+# print(10 + 0.234238378)
+
+# # print( 10 + "hello") #that will give an error
+
+# print( 10 * "hello") # it will print the hello 10 times
+
+# # order of oparation :
+# print( 13 - 3 * 4 + 2 ) # (),*, -, + 
+
+# # variables
+# text_name = 123
+# # text_name = text + 5
+# text_name += 50
+# print(text_name)
+
+# input:
+the_something = input("pleace write something: ")
+print(the_something)
---
+++ greeter_app.py
@@ -0,0 +1,2 @@
+user_name = input("What is your name:")
+print("hello",user_name)
