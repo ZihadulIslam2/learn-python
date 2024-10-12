5d4246b9609069ce6a5d721b967a85e370e7be96
zihadulislam2 <zihadul708@gmail.com>
2024-10-12 11:38:25 +0600

    c3_funciton and methods

---
+++ ex_pythagoras_calculation.py
@@ -0,0 +1,13 @@
+side_a = int(input('the weight:  '))
+side_b = int(input('the hight:   '))
+
+hypotenuse = (side_a**2 + side_b**2) ** (1/2)
+print('The hipotenuse is:  ',round(hypotenuse,2)) 
+
+
+# hight = int(input("what is the hight: "))
+# weight = int(input("what is the weight: "))
+
+# type(sum = pow((hight + weight),2))
+# hypotenuse = sum ** (1/2)
+# print("The result is :  ",sum)
---
+++ function_methods.py
@@ -0,0 +1,20 @@
+# function
+print('a value')
+print(input('ask for input some thing :')) # input return a value so it will shot that.
+
+print(print('this is the print function:'))
+
+
+# methods: => they are function of data types.
+
+print("zihadul islam tusher".upper()) # upper no need a argument.
+
+print("I'LL CHANGE IT TO LOWERCASE".lower())
+
+print('value'.replace('u','o')) # this replace take two argument.1st is what is you looking for, 2nd is what wil be the change.
+
+# other function
+print(abs(-19)) # abs is absolute number.that make a positive number.
+print(max(10,2,49,10,2939))
+print(min(098,8,776,0))
+print(len('test'))
